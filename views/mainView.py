3ec64e3aaf96e1305781266b0cc0714e5e7161c3
alekslu <aleks.luik@gmail.com>
2024-05-08 22:07:30 +0300

    parandus

--- views/mainView.py
+++ views/mainView.py
@@ -38,23 +38,9 @@ def confirm():
                     checked_frame.insert(tk.END, str(checkbox_value) + ' : ' + str(value) + '\n')
             else:
                 checked_frame.insert(tk.END, str(checkbox_value) + ': //Väärtus puudub//')
-            
 
     checked_frame.configure(state='disabled')
 
-    translated_checked_checboxes.clear()
-    checked_frame.configure(state='normal')
-    checked_frame.delete('1.0', tk.END)  # Clear existing content
-    for string_var in checked_checkboxes:
-        print(string_var)
-        text = string_var.get()
-        if text:
-            translated_checked_checboxes.append(text)
-    # Update the checked_frame with the updated list of checked checkboxes
-    for item in translated_checked_checboxes:
-        checked_frame.insert(tk.END, item + '\n')
-    print("translated_checked_checboxes", translated_checked_checboxes)
-    checked_frame.configure(state='disabled')
 #
 window = tk.Tk()
 window.geometry("700x700")
